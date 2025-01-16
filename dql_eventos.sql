ccec0cd9ba9bd38bb3917e3cd83685fb4d1924cc
Alejandra2070 <am3793825@gmail.com>
2025-01-15 21:42:30 -0500

    :sparkles: completion to triggers and four events

--- dql_eventos.sql
+++ dql_eventos.sql
@@ -1,7 +1,76 @@
 USE Universidad;
 
 -- 1. Genera un informe mensual con el total de alumnos matriculados por grado y lo almacena automáticamente.
+create table informe_mensual_matriculas(
+	id int unsigned auto_increment primary key,
+    grado_id int not null,
+    total_alumnos int not null,
+    fecha_informe datetime not null,
+    foreign key(grado_id) references grado(id)
+);
+
+delimiter //
+create event ReporteMensualDeAlumnos
+on schedule every 1 month
+do
+begin
+	insert into informe_mensual_matriculas
+    (grado_id, total_alumnos, fecha_informe)
+    select id_grado, count(id_alumno), NOW()
+    from alumno_se_matricula_asignatura
+    group by id_grado;
+end // 
+delimiter ;
+
 -- 2. Actualiza el total de horas impartidas por cada departamento al final de cada semestre.
+alter table departamento add column horas_impartidas int default 0;
+
+delimiter //
+create event ActualizarHorasDepartamento
+on schedule every 6 month
+do
+begin
+	update departamento d
+    inner join (
+    select p.id_departamento, sum(a.horas) as total_horas
+    from asignatura a
+    inner join profesor p on a.id_profesor = p.id
+    group by p.id_departamento)
+    as horas_totales
+    on d.id = horas_totales.id_departamento
+    set d.horas_impartidas = horas_totales.total_horas;
+end //
+delimiter ;
+
 -- 3. Envía una alerta cuando una asignatura no ha sido cursada en el último año.
+create table alertas (
+	id int primary key auto_increment,
+    id_asignatura int not null,
+    mensaje varchar(255),
+    fecha_alerta datetime,
+    foreign key(id_asignatura) references asignatura(id)
+);
+
+delimiter //
+create event AlertaAsignaturaNoCursadaAnual
+on schedule every 1 year
+do
+begin
+	insert into alertas (id_asignatura, mensaje, fecha_alerta)
+    select a.id, 'La asignatura no ha sido cursada en el último año', now()
+    from asignatura a
+    left join alumno_se_matricula_asignatura am on a.id = am.id_asignatura
+    where am.id_alumno is null and a.cuatrimestre < year(now()) -1;
+end //
+delimiter ;
+
 -- 4. Borra los registros antiguos de auditoría al final de cada semestre.
+delimiter //
+create event LimpiarAuditoriaCadaSemestre
+on schedule every 6 month
+do
+begin
+	delete from auditoria where fecha_creacion < date_sub(now(), interval 6 month);
+end  //
+delimiter ;
 -- 5. Actualiza la lista de profesores destacados al final de cada semestre basándose en evaluaciones y desempeño.
--- dql_triggers.sql
+++ dql_triggers.sql
@@ -1,5 +1,7 @@
 USE Universidad;
 
+alter table profesor add column total_asignaturas int default 0;
+
 -- 1. Al asignar una nueva asignatura a un profesor, 
 -- actualiza el total de asignaturas impartidas por dicho profesor.
 delimiter //
@@ -7,20 +9,134 @@ create trigger ActualizarTotalAsignaturasProfesor
 after insert on asignatura
 for each row
 begin
-    declare total_asignaturas int;
-
-    select count(*) 
-    into total_asignaturas
-    from asignatura 
+	declare total_asignaturas_interna int;
+    -- Obtener el total de asignaturas actuales del docente
+    select count(*) into total_asignaturas_interna
+    from asignatura
     where id_profesor = new.id_profesor;
-
-    update profesor
-    set total_asignaturas = total_asignaturas
+    
+    -- Actualizar el total de asignaturas
+    -- impartidas por el docente
+    update profesor 
+    set total_asignaturas = total_asignaturas_interna
     where id = new.id_profesor;
+    
 end //
 delimiter ;
 
 -- 2. Cada vez que se modifica un registro de un alumno, guarda el cambio en una tabla de auditoría.
+create table auditoria(
+	id_auditoria int unsigned auto_increment primary key,
+    fecha_creacion date not null,
+    accion varchar(255) not null,
+    id_alumno int not null,
+    foreign key(id_alumno) references alumno(id)
+);
+
+delimiter //
+create trigger AuditarActualizacionAlumno
+after update on alumno
+for each row
+begin
+	if old.telefono != new.telefono then
+		insert into auditoria (id_alumno, fecha_creacion, accion)
+		values (new.id, now(), concat('Actualizaste: ', old.telefono, ' -> ', new.telefono));
+	end if;
+end // 
+delimiter ;
+update alumno set telefono = '123456789' where id = 1;
+SELECT * FROM auditoria;
+
 -- 3. Al modificar los créditos de una asignatura, guarda un historial de los cambios.
--- 4. Registra una notificación cuando se elimina una matrícula de un alumno.
+create table historial_creditos (
+	id_historial int unsigned auto_increment primary key,
+    fecha_modificacion date not null,
+    creditos varchar(255) not null,
+    id_asignatura int not null,
+    foreign key(id_asignatura) references asignatura(id)
+);
+
+delimiter //
+create trigger RegistrarHistorialCreditos
+after update on asignatura
+for each row
+begin
+	if old.creditos != new.creditos then
+		insert into historial_creditos (fecha_modificacion, creditos, id_asignatura)
+        values (now(), concat('Actualizaste: ', old.creditos, ' -> ', new.creditos), new.id);
+	end if;
+end // 
+delimiter ;
+update asignatura set creditos = '7' where id = 1;
+select * from historial_creditos;
+
+-- 4. Registra una notificación cuando se elimina una matrícula de un alumno. id, idalumno,fechacancelacion,motivocancelacion
+create table notificaciones (
+	id int unsigned auto_increment primary key,
+    fecha_cancelacion date not null,
+    motivo varchar(255) not null,
+    id_alumno int not null,
+    id_asignatura int not null,
+    id_curso_escolar int not null,
+    foreign key(id_alumno) references alumno(id),
+    foreign key(id_asignatura) references asignatura(id),
+    foreign key(id_curso_escolar) references curso_escolar(id)
+);
+
+-- drop trigger NotificarCancelacionMatricula;
+delimiter //
+create trigger NotificarCancelacionMatricula
+after delete on alumno_se_matricula_asignatura
+for each row
+begin
+	insert into notificaciones (fecha_cancelacion, motivo, id_alumno, id_asignatura, id_curso_escolar)
+    values (now(), 'Matrícula cancelada', old.id_alumno, old.id_asignatura, old.id_curso_escolar);
+end // 
+delimiter ; 
+
+delete from alumno_se_matricula_asignatura where id_alumno = 1 and id_asignatura = 3 and id_curso_escolar = 1;
+select * from notificaciones;
+
 -- 5. Evita que un profesor tenga más de 10 asignaturas asignadas en un semestre.
+delimiter //
+create trigger RestringirAsignacionExcesiva
+before insert on asignatura
+for each row
+begin
+	declare cant_asignaturas int;
+    
+    select count(*) into cant_asignaturas from asignatura
+    where id_profesor = new.id_profesor and cuatrimestre = new.cuatrimestre;
+    
+    if cant_asignaturas >= 10 then
+		signal sqlstate '45000' set message_text = 'El profesor no puede tener más de 10 asignaturas asignadas en un semestre';
+    end if;
+end  //
+delimiter ;
+
+insert into asignatura (id, nombre, creditos, tipo, curso, cuatrimestre, horas, id_profesor, id_grado)
+values (84, 'Matemáticas I', 6, 'básica', 1, 1, 30, 3, 1);
+
+insert into asignatura (id, nombre, creditos, tipo, curso, cuatrimestre, horas, id_profesor, id_grado)
+values (85, 'Matemáticas I', 6, 'básica', 1, 1, 30, 3, 1);
+
+insert into asignatura (id, nombre, creditos, tipo, curso, cuatrimestre, horas, id_profesor, id_grado)
+values (86, 'Matemáticas I', 6, 'básica', 1, 1, 30, 3, 1);
+
+insert into asignatura (id, nombre, creditos, tipo, curso, cuatrimestre, horas, id_profesor, id_grado)
+values (87, 'Matemáticas I', 6, 'básica', 1, 1, 30, 3, 1);
+
+insert into asignatura (id, nombre, creditos, tipo, curso, cuatrimestre, horas, id_profesor, id_grado)
+values (88, 'Matemáticas I', 6, 'básica', 1, 1, 30, 3, 1);
+
+insert into asignatura (id, nombre, creditos, tipo, curso, cuatrimestre, horas, id_profesor, id_grado)
+values (89, 'Matemáticas I', 6, 'básica', 1, 1, 30, 3, 1);
+
+insert into asignatura (id, nombre, creditos, tipo, curso, cuatrimestre, horas, id_profesor, id_grado)
+values (90, 'Matemáticas I', 6, 'básica', 1, 1, 30, 3, 1);
+
+insert into asignatura (id, nombre, creditos, tipo, curso, cuatrimestre, horas, id_profesor, id_grado)
+values (91, 'Matemáticas I', 6, 'básica', 1, 1, 30, 3, 1);
+
+insert into asignatura (id, nombre, creditos, tipo, curso, cuatrimestre, horas, id_profesor, id_grado)
+values (92, 'Matemáticas I', 6, 'básica', 1, 1, 30, 3, 1);
